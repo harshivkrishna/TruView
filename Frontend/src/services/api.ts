fd2ae930fa298780a60bfd87dc9c304c37986b30
harshivkrishna <harshivkrishnam.cse2023@citchennai.net>
2025-08-19 19:35:39 +0530

    modified:   Frontend/src/services/api.ts

--- Frontend/src/services/api.ts
+++ Frontend/src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { getAnonymousId } from '../utils/anonymousId';
 
-const API_BASE_URL = 'http://localhost:5000/api';
+const API_BASE_URL = 'https://truview-y909.onrender.com/api';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
